2fe4e899b4b5214d8d54e0d11785109afe576e14
Abolfazl Ganjtabesh <brillianttrussure@gmail.com>
2026-05-10 14:00:23 +0330

    feat(repository-json): implement JSON-based UserRepository
    - add file-based UserRepositoryJson implementation
    - persist users in local users.json file
    - implement create method with incremental id generation
    - implement getUserById method for user retrieval
    - encapsulate file read/write operations with private helpers

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,6 +1,6 @@
 const CreateUser = require("../use-cases/user/create-user")
 const GetUserById = require("../use-cases/user/get-user-by-id")
-const UserRepository = require("../repository/postgre/user-repository")
+const UserRepository = require("../repository/json/user-repository")
 
 const userRepository = new UserRepository()
 
@@ -51,5 +51,5 @@ module.exports = {
       return res.status(404).json({ error: error.message });
     }
   }
-  
+
 }
---
+++ repository/json/user-repository.js
@@ -0,0 +1,47 @@
+//repository/json/user-repository
+const fs = require("fs").promises;
+const path = require("path");
+
+class UserRepositoryJson {
+    constructor() {
+        this.filePath = path.join(__dirname, "../../data/users.json");
+    }
+
+    async _readFile() {
+        try {
+            const data = await fs.readFile(this.filePath, "utf-8");
+            return JSON.parse(data);
+        } catch (error) {
+            return [];
+        }
+    }
+
+    async _writeFile(data) {
+        await fs.writeFile(this.filePath, JSON.stringify(data, null, 2));
+    }
+
+    async create(user) {
+        const users = await this._readFile();
+
+        const newUser = {
+            ...user,
+            id: users.length ? users[users.length - 1].id + 1 : 1
+        };
+
+        users.push(newUser);
+
+        await this._writeFile(users);
+
+        return newUser;
+    }
+
+    async getUserById(id) {
+        const users = await this._readFile();
+
+        const user = users.find(u => u.id === Number(id));
+
+        return user || null;
+    }
+}
+
+module.exports = UserRepositoryJson;
