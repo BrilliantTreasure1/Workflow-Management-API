58f08989050cbf1e83528921b8a13530a0856de9
Abolfazl Ganjtabesh <brillianttrussure@gmail.com>
2026-05-11 01:57:33 +0330

    feat(routes): add workflow creation route
    - register POST /users/:userId/workflows endpoint
    - protect route with auth middleware
    - connect route to workflowController.create
    - organize user, auth, and workflow routes in Express app

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express")
 const userController = require("./controller/user-controller")
 const authController = require("./controller/auth-controller")
 const authMiddleware = require("./middleware/auth")
+const workflowController = require("./controller/workflow-controller")
+
 
 
 
@@ -13,6 +15,9 @@ app.post('/users', userController.create);
 app.get('/users/:id', authMiddleware,userController.getById);
 
 app.post('/auth/login', authController.login);
+
+app.post('/users/:userId/workflows', authMiddleware , workflowController.create);
+
 
 
  app.listen(3000, () => {
