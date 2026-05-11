4c803e23e89b5d7d963b080483c5aafa890bb021
Abolfazl Ganjtabesh <brillianttrussure@gmail.com>
2026-05-11 10:48:20 +0330

    feat(workflow-controller): add endpoint to list user workflows
    - integrate FindWorkflowsByUser use case in workflow controller
    - implement findByUser method to retrieve workflows for authenticated user
    - obtain userId from auth middleware (req.user)
    - return 200 response with list of workflows
    - handle validation and use case errors with 400 response

--- controller/workflow-controller.js
+++ controller/workflow-controller.js
@@ -1,10 +1,14 @@
 // controllers/workflow-controller.js
 
 const CreateWorkflow = require("../use-cases/workflow/create-workflow");
+const FindWorkflowsByUser = require("../use-cases/workflow/find-workflow-by-user")
+
 const WorkflowRepository = require("../repository/postgre/workflow-repository");
 
 const workflowRepository = new WorkflowRepository();
 const createWorkflowUseCase = new CreateWorkflow(workflowRepository);
+const findWorkflowsByUserUseCase = new FindWorkflowsByUser(workflowRepository)
+
 
 module.exports = {
 
@@ -32,6 +36,19 @@ module.exports = {
       return res.status(500).json({ error: error.message });
 
     }
+  },
+
+  async findByUser(req, res) {
+  try {
+    const userId = req.user.userId;
+
+    const workflows = await findWorkflowsByUserUseCase.execute({ userId });
+
+    return res.status(200).json(workflows);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
   }
+}
+
 
 };
